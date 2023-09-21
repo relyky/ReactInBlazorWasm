d42e24d081eb78ddfec6863de4292bd7087d9f8a
Rely Ky <relyky@hotmail.com>
2023-09-21 20:57:34 +0800

    與 React 元件通訊

--- React/src/MyCounter.js
+++ React/src/MyCounter.js
@@ -1,13 +1,19 @@
 ﻿import React, { useState } from 'react'
 
-export default function MyCounter({ initCount }) {
+export default function MyCounter({ initCount /* int */, onChange /* event */ }) {
   const [count, setCount] = useState(initCount || 0);
+
+  function HandleClick() {
+    const newCount = count + 1;
+    setCount(newCount)
+    onChange(newCount)
+  }
 
   return (
     <div style={{ border: 'solid 2px red', borderRadius: 8, padding: 8 }}>
       <h3>我用 React 開發出來的</h3>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
+      <button onClick={HandleClick}>
         Click me
       </button>
     </div>
--- React/src/index.js
+++ React/src/index.js
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom/client'
 import MyCounter from './MyCounter'
 import MyTitle from './MyTitle'
 
-// 加入更新此元件函式
-window.renderMyCounter = function (id, initCount) {
-  const root = ReactDOM.createRoot(document.getElementById(id));
-  root.render(<MyCounter initCount={initCount} />);
+//## 單向繫結 React 元件：MyTitle
+window.renderMyTitle = function (rootElement, title) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<MyTitle title={title} />);
 }
 
-// 加入更新此元件函式
-window.renderMyTitle = function (id, title) {
-  const root = ReactDOM.createRoot(document.getElementById(id));
-  root.render(<MyTitle title={title} />);
+//## 雙向繫結 React 元件：MyCounter
+window.renderMyCounter = function (dotNetObject, rootElement, initCount) {
+  function handleChange(newCount) {
+    // events up
+    dotNetObject.invokeMethodAsync('OnCountChange', newCount);
+    console.log(`你變了 => ${newCount}`);
+  }
+
+  // props down
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<MyCounter initCount={initCount} onChange={handleChange} />);
+  dotNetObject.invokeMethodAsync('OnCountChange', initCount); // init notify.
 }
