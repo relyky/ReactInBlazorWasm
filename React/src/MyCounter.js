6c0308fdf42226d27383705dd5d4ff262cfe872d
Rely Ky <relyky@hotmail.com>
2023-09-21 18:28:12 +0800

    第一個 React 元件

---
+++ React/src/MyCounter.js
@@ -0,0 +1,15 @@
+﻿import React, { useState } from 'react'
+
+export default function MyCounter({ initCount }) {
+  const [count, setCount] = useState(initCount || 0);
+
+  return (
+    <div style={{ border: 'solid 2px red', borderRadius: 8, padding: 8 }}>
+      <h3>我用 React 開發出來的</h3>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  )
+}
---
+++ React/src/index.js
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MyCounter from './MyCounter'
+
+
+// 加入更新此元件函式
+window.renderMyCounter = function (id, initCount) {
+  const root = ReactDOM.createRoot(document.getElementById(id));
+  root.render(<MyCounter initCount={initCount} />);
+}
