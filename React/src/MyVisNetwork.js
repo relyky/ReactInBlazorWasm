82bdba9bdfdb6e304516d5ce7056ea4a2a3245eb
Rely Ky <relyky@hotmail.com>
2023-09-28 16:24:53 +0800

    vis-network sample - 2nd sample

--- React/src/MyVisNetwork.js
+++ React/src/MyVisNetwork.js
@@ -1,60 +1,14 @@
 ﻿import React, { useEffect, useRef } from "react"
-import { Network } from "vis-network"
+import VisNetworkLab1 from './VisNetworkSample/VisNetworkLab1'
+import VisNetworkLab2 from './VisNetworkSample/VisNetworkLab2'
 
 export default function MyVisNetwork({ title }) {
 
   return (
     <div className="p-2 my-2" style={{ border: 'solid 2px red', borderRadius: 8 }}>
       <h3>{title}</h3>
-      <VisNetwork />
+      <VisNetworkLab1 />
+      <VisNetworkLab2 />
     </div>
-  )
-}
-
-const VisNetwork = () => {
-  const visRef = useRef(null);
-
-  const nodes = [
-    { id: 1, label: "Node 1", title: "tool tip 1" },
-    { id: 2, label: "節點２", title: "tool tip 2" },
-    { id: 3, label: "Node 3", title: "tool tip 3" },
-    { id: 4, label: "節點四", title: "tool tip 4" },
-    { id: 5, label: "Node 5", title: "tool tip 5" },
-    { id: 6, label: "第六點", title: "tool tip 6" },
-  ];
-
-  const edges = [
-    { from: 1, to: 3 },
-    { from: 1, to: 2 },
-    { from: 2, to: 4 },
-    { from: 2, to: 5 },
-    { from: 3, to: 3 },
-    { from: 1, to: 5 },
-  ];
-
-  const data = {
-    nodes: nodes,
-    edges: edges,
-  };
-
-  const options = {
-    height: "400px",
-    //layout: {
-    //  hierarchical: true
-    //},
-    //edges: {
-    //  color: "#000000"
-    //},
-  };
-
-  useEffect(() => {
-    const network = new Network(visRef.current, data, options);
-    // Use `network` here to configure events, etc
-  }, [visRef, nodes, edges, options]);
-
-  return (
-    <div ref={visRef} style={{
-      border:'solid 1px darkgrey'
-    }} />
   )
 }
---
+++ React/src/VisNetworkSample/VisNetworkLab2.js
@@ -0,0 +1,79 @@
+﻿import React, { useEffect, useRef } from "react"
+import { Network } from "vis-network"
+
+export default function VisNetworkLab2() {
+  const visRef = useRef(null);
+
+  const nodes = [
+    { id: 11, label: "約翰", group: "Person" },   // John
+    { id: 12, label: "瑪莉", group: "Person" },   // Mary
+    { id: 13, label: "愛麗絲", group: "Person" }, // Alice
+    { id: 14, label: "雅各", group: "Person" },   // Jacob
+    { id: 15, label: "茱麗葉", group: "Person" }, // Julie
+
+    { id: 21, label: "塔克戴爾", group: "Restaurant" }, // Taco Dell
+    { id: 22, label: "薑和香料", group: "Restaurant" }, // Ginger and Spice
+    { id: 23, label: "麵條樂園", group: "Restaurant" }, // Noodle Land
+
+    { id: 31, label: "貝爾維尤", group: "City" }, // Bellevue
+    { id: 32, label: "西雅圖", group: "City" }, // Seattle
+    { id: 33, label: "雷德蒙", group: "City" }, // Redmond
+  ];
+
+  const edges = [
+    { from: 11, to: 21, group: "likes", label: "喜歡", arrows: 'to' },
+    { from: 12, to: 22, group: "likes", label: "喜歡", arrows: 'to' },
+    { from: 13, to: 23, group: "likes", label: "喜歡", arrows: 'to' },
+    { from: 14, to: 23, group: "likes", label: "喜歡", arrows: 'to' },
+    { from: 15, to: 21, group: "likes", label: "喜歡", arrows: 'to' },
+
+    { from: 11, to: 31, group: "livesIn", label: "住在" },
+    { from: 12, to: 32, group: "livesIn", label: "住在" },
+    { from: 13, to: 33, group: "livesIn", label: "住在" },
+    { from: 14, to: 33, group: "livesIn", label: "住在" },
+    { from: 15, to: 33, group: "livesIn", label: "住在" },
+
+    { from: 21, to: 31, group: "locatedIn", label: "位在" },
+    { from: 22, to: 32, group: "locatedIn", label: "位在" },
+    { from: 23, to: 33, group: "locatedIn", label: "位在" },
+
+    { from: 11, to: 12, group: "friendOf", label: "朋友" },
+    { from: 12, to: 13, group: "friendOf", label: "朋友" },
+    { from: 13, to: 11, group: "friendOf", label: "朋友" },
+    { from: 14, to: 12, group: "friendOf", label: "朋友" },
+    { from: 15, to: 14, group: "friendOf", label: "朋友" },
+  ];
+
+  const data = {
+    nodes: nodes,
+    edges: edges,
+  };
+
+  const options = {
+    height: "800px",
+    //groups: {
+    //  City: { borderWidth: 2 },
+    //  Restaurant: { color: { background: 'pink', border: 'red' }, borderWidth: 3 },
+    //},
+    //layout: {
+    //  hierarchical: true
+    //},
+    //edges: {
+    //  color: "#000000"
+    //},
+  };
+
+  useEffect(() => {
+    const network = new Network(visRef.current, data, options);
+    // Use `network` here to configure events, etc
+  }, [visRef, nodes, edges, options]);
+
+  return (
+    <div>
+      <h4>VisNetworkLab2</h4>
+      <div ref={visRef} style={{
+        border: 'solid 1px darkgrey'
+      }} />
+    </div>
+  )
+}
