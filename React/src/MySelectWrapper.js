0bd9a788a38bb635fac24499784009667e9b6d0a
Rely Ky <relyky@hotmail.com>
2023-10-03 11:12:13 +0800

    導入 mediator.js - MySelect2 - commit

---
+++ React/src/MySelectWrapper.js
@@ -0,0 +1,45 @@
+﻿import React, { useState, useEffect } from 'react'
+import Select from 'react-select'
+
+//const options = [
+//  { value: 'chocolate', label: 'Chocolate' },
+//  { value: 'strawberry', label: 'Strawberry' },
+//  { value: 'vanilla', label: 'Vanilla' }
+//]
+
+/**
+ * props := { 
+ *   dotNetObject,
+ *   channel: MediatorChannel, 
+ *   value 
+ *   options, 
+ * }
+ */
+
+export default function MySelectWrapper({ dotNetObject, channel, options, value }) {
+  const [_options, setOptions] = useState(options);
+  const [_value, setValue] = useState(value);
+
+  useEffect(() => {
+    // 註冊通訊
+    window.__mediator.subscribe(channel, (payload) => {
+      //## update props
+      const { value: newValue, options: newOptions } = payload
+      if (!!newValue) setValue(newValue)
+      if (!!newOptions) setOptions(newOptions)
+    });
+    return () => {
+      // 解除註冊通訊
+      window.__mediator.remove(channel)
+    }
+  }, [])
+
+  //## events up with dotNetObject
+  function handleChange(selectedOption /* LabelValue */) {
+    dotNetObject.invokeMethodAsync('OnSelect', selectedOption);
+  }
+
+  return (
+    <Select options={_options} onChange={handleChange} value={_value} />
+  )
+}
--- React/src/index.js
+++ React/src/index.js
@@ -1,19 +1,19 @@
-﻿import React, { createRef } from 'react'
+﻿import React from 'react'
 import ReactDOM from 'react-dom/client'
 import r2wc from "@r2wc/react-to-web-component"
 import BlackScreen from './BlackScreen'
-import { v4 as uuidv4 } from 'uuid'
+// import { v4 as uuidv4 } from 'uuid'
 import MyCounter from './MyCounter'
 import MyTitle from './MyTitle'
 import MySelect from './MySelect'
 import MyQRCode from './MyQRCode'
 import MyAcquisitionsChart from './MyAcquisitionsChart'
 import MyDimensionsChart from './MyDimensionsChart'
-import MyMiniApp from './MyMiniApp'
-import MySelect2 from './MySelect2'
+import MySelectWrapper from './MySelectWrapper'
 import MyVisNetwork from './MyVisNetwork'
 
-window.MyReactComponentsRepo = {}
+//※ 導入 mediator 以實現 Blazor 與 React 進階通訊。
+window.__mediator = window.Mediator();
 
 //#region 只能註冊簡單的 Web Componnets
 //※ 只能 props-down 尚無法 events-up。
@@ -69,6 +69,21 @@ window.renderMySelect = function (dotNetObject, rootElement, options, value) {
   root.render(<MySelect options={options} onChange={handleChange} value={value} />);
 }
 
+//#region ## 註冊進階元件: MySelect Adv : react-select
+
+window.renderMySelect2 = function (dotNetObject, elementId, options, value) {
+  //注意：此例 elementId 也是 channelId。將透過 mediator 間接通訊。
+  const rootElement = document.getElementById(elementId)
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <React.StrictMode>
+      <MySelectWrapper dotNetObject={dotNetObject} channel={elementId} options={options} value={value} />
+    </React.StrictMode>
+  );
+}
+
+//#endregion
+
 //## 註冊單向繫結 React 元件：qrcode.react
 window.renderMyQRCode = function (rootElement, code) {
   const root = ReactDOM.createRoot(rootElement);
@@ -87,85 +102,7 @@ window.renderMyDimensionsChart = function (rootElement, title, dataSets) {
   root.render(<MyDimensionsChart title={title} dataSets={dataSets} />);
 }
 
-//#region ## 註冊進階元件: MyMiniApp
-window.renderMyMiniApp = function (rootElement, initAttrs) {
-  const root = ReactDOM.createRoot(rootElement)
-  const rootRef = createRef()
-
-  this.updateAttrs = (newAttrs) => {
-    rootRef.current.updateAttrs(newAttrs)
-  }
-
-  function handleChange(attrs) {
-    console.log('MyMiniApp.handleChange =>', { attrs })
-    // 需要上送訊息的話，可透過 dotNetObject.invokeMethodAsync() 往上送訊息。
-  }
-
-  root.render(<MyMiniApp ref={rootRef} attrs={initAttrs} onChange={handleChange} />)
-
-  const miniAppUid = `MyMiniApp-${uuidv4()}`
-  window.MyReactComponentsRepo[miniAppUid] = this
-  return miniAppUid
-}
-
-window.updateMyMiniApp = function (miniAppUid /* string */, newAttrs /* object */) {
-  const miniApp = window.MyReactComponentsRepo[miniAppUid]
-  miniApp.updateAttrs(newAttrs)
-}
-
-//#endregion
-
-//#region ## 註冊進階元件: MySelect Adv : react-select
-window.renderMySelect2 = function (dotNetObject, rootElement, options, value) {
-  //const options = [
-  //  { value: '焦糖瑪奇朵', label: '焦糖瑪奇朵' },
-  //  { value: '阿華田', label: '阿華田' },
-  //  { value: 'chocolate', label: 'Chocolate' },
-  //  { value: 'strawberry', label: 'Strawberry' },
-  //  { value: 'vanilla', label: 'Vanilla' }
-  //]
-
-  const root = ReactDOM.createRoot(rootElement)
-  const rootRef = createRef()
-
-  this.updateAttrs = (newAttrs) => {
-    //console.log(`renderMySelect2.updateAttrs`);
-    const { options, value } = newAttrs
-
-    if (options && Array.isArray(options)) {
-      rootRef.current.updateOptions(options)
-    }
-
-    if (value === null) {
-      rootRef.current.updateValue(value)
-    }
-    else if (value instanceof Object) {
-      rootRef.current.updateValue(value)
-    }
-  }
-
-  function handleChange(selectedOption /* LabelValue */) {
-    //console.log(`MySelect2.OnSelect =>`, selectedOption);
-    dotNetObject.invokeMethodAsync('OnSelect', selectedOption);
-  };
-
-  //console.log(`MySelect2.render =>`, value);
-  root.render(<MySelect2 ref={rootRef} options={options} onChange={handleChange} value={value} />);
-
-  const miniAppUid = `MySelect2-${uuidv4()}`
-  window.MyReactComponentsRepo[miniAppUid] = this
-  return miniAppUid
-}
-
-window.updateMySelect2 = function (miniAppUid /* string */, newAttrs /* object */) {
-  console.log(`updateMySelect2 =>`, newAttrs);
-  const miniApp = window.MyReactComponentsRepo[miniAppUid]
-  miniApp.updateAttrs(newAttrs)
-}
-
-//#endregion
-
-//#region ## 註冊元件: vis-network 測試 
+//## 註冊元件: vis-network 測試 
 window.renderMyVisNetwork = function (rootElement, title) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
